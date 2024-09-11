23ec39ee5215bcff98224287111b91ee7ebdb727
Emeghiebo Chizoba <chizobaemeh09@gmail.com>
2024-09-11 02:28:58 +0100

    created 90% of the website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    screens: {
+      sm: '480p',
+      md: '768px',
+      lg: '1100px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}

a5b818ed94370d56ea3ce2937eca1892be472569
Emeghiebo Chizoba <chizobaemeh09@gmail.com>
2024-11-12 08:18:03 +0100

    changed UI elements

--- js/main.js
+++ js/main.js
@@ -25,9 +25,9 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
-  autoplay: {
-    delay: 5000,
-  },
+  // autoplay: {
+  //   delay: 5000,
+  // },
 
   effect: 'fade',
   fadeEffect: {
